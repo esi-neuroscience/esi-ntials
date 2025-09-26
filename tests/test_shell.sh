53aa4cd4a3a6d18ab2aa009347e15a63064eb299
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2025-09-26 12:57:10 +0200

    NEW: Added logging example test
    - test correct functionality of logging example
    
    On branch main
    Changes to be committed:
    	modified:   tests/test_shell.sh

--- tests/test_shell.sh
+++ tests/test_shell.sh
@@ -20,7 +20,7 @@ tmpout="${shellFolder}/tmp.txt"
 # Clean up remains of potentially failed previous test runs
 rm -f "${tmpout}"
 
-# Start testing pipeline
+# ----------------------------------------------------------------------
 info "Testing load_params.sh...."
 "${shellFolder}/load_params.sh" > "${tmpout}"
 
@@ -61,5 +61,57 @@ if [[ -n "${fail-}" ]]; then
 fi
 
 passed "Executed load_params.sh successfully"
+
+# ----------------------------------------------------------------------
+info "Testing log_msg.sh...."
+log="./tmp.log"
+rm -f "${log}"
+
+"${shellFolder}/log_msg.sh" > "${tmpout}"
+
+debug "Ensure that the initial debug message was not printed"
+grep -qw "This message is not shown" "${tmpout}"
+if [ $? -eq 0 ]; then
+    warn "Debug message not hidden"
+    fail=1
+fi
+grep -qw "${datename}.*=== An info message" "${tmpout}"
+if [ $? -ne 0 ]; then
+    warn "First INFO message not shown"
+    fail=1
+fi
+grep -qw "${datename}.*WARNING: A warning message" "${tmpout}"
+if [ $? -ne 0 ]; then
+    warn "Initial warning message not shown"
+    fail=1
+fi
+grep -qw ">>> A big announcement! <<<" "${tmpout}"
+if [ $? -ne 0 ]; then
+    warn "Announcement not shown"
+    fail=1
+fi
+grep -qw "${datename}.*DEBUG: This message is shown" "${tmpout}"
+if [ $? -ne 0 ]; then
+    warn "Debug message not shown despite enabled 'debugflag'"
+    fail=1
+fi
+grep -qw "${datename}.*=== This messages is printed and logged to ./tmp.log" "${tmpout}"
+if [ $? -ne 0 ]; then
+    warn "Second info message not shown"
+    fail=1
+fi
+grep -qw "${datename}.*ERROR: Crash with an error" "${tmpout}"
+if [ $? -ne 0 ]; then
+    warn "Final error message not shown"
+    fail=1
+fi
+
+grep -qf ./tmp.log shell/tmp.txt
+if [ $? -ne 0 ]; then
+    warn "Log file not populated"
+    fail=1
+fi
+
+passed "Executed logging example successfully"
 
 exit 0
