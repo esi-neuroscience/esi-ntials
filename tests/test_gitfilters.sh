61b0c3aab7bc06ef4e622e961edb8302243a4c0a
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2026-03-18 16:45:22 +0100

    FIX: Repaired file-path setup
    - use absolute paths to make GitHub action work
    
    On branch dev
    Changes to be committed:
    	modified:   tests/test_gitfilters.sh

--- tests/test_gitfilters.sh
+++ tests/test_gitfilters.sh
@@ -13,12 +13,16 @@ set -e
 thisFolder=`dirname "$(readlink -f "$0")"`
 source "${thisFolder}/tests.setup"
 
+# Get repo directory structure
+rootFolder=`dirname ${thisFolder}`
+gitexamplefolder="${rootFolder}/config/git_filters"
+
 # Target location for dummy repo
 testdir="${HOME}/test-repo"
 
 # Create and populate dummy repo
 mkdir "${testdir}"
-cp -r ../config/git_filters/. "${testdir}/"
+cp -r "${gitexamplefolder}/." "${testdir}/"
 
 # Initialize empty git repo and apply + test filter
 pushd "${testdir}" 1>/dev/null
@@ -32,8 +36,9 @@ git add settings.json
 git commit -m "test commit"
 git cat-file -p HEAD:settings.json | grep -Fq "api-key" || error "Key has not been filtered!" | exit 1
 cat settings.json  | grep -Fq "my_super_secret_key" || error "Key was removed from original file!" | exit 1
-info "SUCCESS!"
 popd 1>/dev/null
+
+passed "Git filter has been applied successfully"
 
 # Cleanup
 rm -rf "${testdir}"
